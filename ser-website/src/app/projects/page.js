048da751028280298678bb3b5c9644b45ff8880e
GEE <gm.walker2022@gmail.com>
2023-12-03 01:06:09 -0500

    <feat> Massive Updates
    - Reconstructed the website + minor tweaks.
    - Created not-found.js

--- ser-website/src/app/projects/page.js
+++ ser-website/src/app/projects/page.js
@@ -1,10 +1,32 @@
+import Navbar from "@/components/navbar";
+
 const projects = () =>{
-    return (
-        <main>
-            <body>
-                <h1>Projects</h1>
-            </body>
-        </main>
+    return (       
+    <main className="flex min-h-screen w-full flex-col items-center overflow-clip">
+        <Navbar></Navbar>
+        <div>
+
+          <h1>Current Projects</h1>
+
+        <div className="grid-container-element">
+          <h1 className="grid-child-element">SERC website</h1>
+          <h1 className="grid-child-element">rust reticulum</h1>
+        </div>
+
+        <div className="grid-container-element">
+
+        </div>
+
+        <div className="grid-container-element">
+          <div className="grid-child-element">the most vital element of an organization is recognizability. we are currently in development of a visually pleasing, informative website that caters to anyone who may want to know more about what we do. </div>
+
+          <div className="grid-child-element">Reticulum is designed to give communities and users a way of setting up peer-to-peer messaging networks that are secure, private, and extremely resilient. It runs over any kind of hardware --packet radio, LAN, wired connections, or even at the applications layer of TCP/IP</div>
+        </div>
+
+      </div>
+      
+      
+    </main>
     )
 }
 
