54a4e0458b6c148ea075e45d22fdcd0be6105776
GEE <gm.walker2022@gmail.com>
2023-12-08 18:23:04 -0500

    <feat> Updated Navbar Component
    - Added Page Nav. links
    - Improved the casing of Home Button text

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,14 +8,14 @@ export default function Navbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Homepage</a></li>
-                        <li><a href={"https://github.com/AWKohler/SERC-Website"} >SRC</a></li>
+                        <li><a href={"/projects"}>Projects</a></li>
+                        <li><a href={"https://github.com/LibertyUniversitySER/website"} >SRC</a></li>
                         <li><a href={"https://discord.gg/SMcAMwxPs5"}>Discord</a></li>
                     </ul>
                 </div>
             </div>
             <div className="navbar-center">
-                <a className="btn btn-ghost normal-case text-xl">Software engineering readiness club</a>
+                <a className="btn btn-ghost normal-case text-xl" href="/">Software Engineering Readiness Club</a>
             </div>
             <div className="navbar-end">
 
