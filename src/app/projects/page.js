9a511dda86504a06eb5e8a1d42b43ab748099069
GEE <gm.walker2022@gmail.com>
2023-12-08 18:25:09 -0500

    <feat> Updated Projects Page
    - Added basic content

---
+++ src/app/projects/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+
+export default function Projects(){
+    return(
+        <main>
+            <Navbar/>
+            
+            <h1>Projects</h1>
+        </main>
+    )
+}
