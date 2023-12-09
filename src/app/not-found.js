2ee4ae3a2284560bbeb91217212a221234e72dd5
GEE <gm.walker2022@gmail.com>
2023-12-09 02:41:43 -0500

    <feat> Updated ERR page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,11 @@
-import { notFound } from "next/navigation"
 
-notFound = () =>{
+
+export default function NotFound (){
     return(
         <main>
-            <h2>404 ERR</h2>
-            <p>The resource you are looking for doesn`&#39`t exist.</p>
+            <h2 className="">404 ERR</h2>
+            <p>The resource you are looking for doesn&#39;t exist.</p>
         </main>
     )
 }
 
-export default notFound;
