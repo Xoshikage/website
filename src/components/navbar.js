eba4d87996d083178f8c4914f85dc3281dcd877b
GEE <gm.walker2022@gmail.com>
2023-12-10 03:04:51 -0500

    <feat> Updated Navbar Component
    - Changed <header> to <div>

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@
 export default function Navbar() {
 
     return (
-        <header className="navbar bg-base-100">
+        <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost btn-circle text-base-content">
@@ -44,11 +44,15 @@ export default function Navbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
                         <span className="badge badge-xs badge-primary indicator-item"></span>
                     </div>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 bg-base-100 text-base-content">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 bg-base-100 text-base-content space-y-5">
+                        <li>Insert Events Widget Here...</li>
+                        <li>Insert Events Widget Here...</li>
+                        <li>Insert Events Widget Here...</li>
+                        <li>Insert Events Widget Here...</li>
                         <li>Insert Events Widget Here...</li>
                     </ul>
                 </button>
             </div>
-        </header>
+        </div>
     )
 }
