ddbfa178c45a00366f930c5a8f359c29eb77e1eb
4ubiks <jaharris20@liberty.edu>
2024-02-20 17:49:18 -0500

    Projects!

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,33 +1,48 @@
 import Navbar from "@/components/navbar";
+import Hero from "@/components/hero";
 
 export default function Projects(){
     return(
         <main>
             <Navbar></Navbar>
-            <div>               
-                <body>
-        
-                     <a className="titles"><code>current projects for our club</code></a>
-        
-                <div className="grid-container-element">
-                     <h1 className="grid-child-element">SERC website</h1>
-                     <h1 className="grid-child-element">rust reticulum</h1>
-                </div>
-        
-                     <div className="grid-container-element">
-        
-                     </div>
-        
-                 <div className="grid-container-element">
-                     <div className="grid-child-element">the most vital element of an organization is recognizability. we are currently in development of a visually pleasing, informative website that caters to anyone who may want to know more about what we do. </div>
-        
-                     <div className="grid-child-element">Reticulum is designed to give communities and users a way of setting up peer-to-peer messaging networks that are secure, private, and extremely resilient. It runs over any kind of hardware --packet radio, LAN, wired connections, or even at the applications layer of TCP/IP</div>
-                </div>              
-        
-                </body>
-        
-            </div>
+            <div class="w-full">      
+            <Hero className={"min-h-screen h-full"}></Hero>         
+                
+            <div className={"bg-black w-full flex justify-center py-32"}>
 
+                <div className={"w-11/12 max-w-[1500px] grid grid-rows-3 gap-3.5"}>
+                    <div className={"row-start-1"}>
+                        <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center">
+                            <div className="avatar">
+                                <div className="w-72 rounded">
+                                    <img src="https://v2.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt=""/>
+                                </div>
+                            </div>
+
+                            <div className={"ml-8 h-full py-8 prose lg:prose-md"}>
+                                <h1>
+                                    Person
+                                </h1>
+
+
+                                <p>
+                                    This is a person who is in da club
+                                </p>
+                                    {/*<a href={"/projects"}/>*/}
+                            </div>
+
+                                <a href={"/projects"}>link</a>
+
+                        </div>
+                    </div>
+                </div>
+   
+
+
+
+
+            </div>
+            </div>
         </main>
     )
 }
