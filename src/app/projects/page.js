e53b52a15baae17c1fe3fbb20c1b15b3f139f55f
4ubiks <jaharris20@liberty.edu>
2024-02-27 16:34:41 -0500

    projects are epic

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,36 +9,33 @@ export default function Projects(){
 
           <div className="w-full"> 
               <Hero className={"min-h-screen h-full"}></Hero>
-              <div className={"w-full flex justify-center py-32"}>
+              <div className={"bg-gray-800 w-full flex justify-center py-32"}>
 
                   <div className={"w-11/12 max-w-[2500px] grid grid-rows-3 gap-3.5"}>
                       <div className={"row-start-1"}>
                           <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center">
                               <div className="avatar">
                                   <div className="w-72 rounded">
-                                      <img src="" />
+                                      <img src="../img/rust.png"/>
                                   </div>
                               </div>
 
-                              <div className={"ml-8 h-full py-8 prose lg:prose-md"}>
+                              <div className={"ml-8 h-full py-12 prose lg:prose-md"}>
                                   <h1 class="text-5xl">
-                                    Club Website
+                                    Club Website                            
                                   </h1>
 
-                                  <p class="text-2xl text-gray-200">the most vital element of an organization is recognizability.</p>
-                                  <p class="text-2xl">we are currently in development of a visually pleasing, informative website</p>
-                                  <p class="text-2xl">that caters to anyone who may want to know more about what we do.</p>
+                                  <p class="text-2xl text-gray-200 py-8 ">the most vital element of an organization is recognizability. we are currently in development of a visually pleasing, informative website that caters to anyone who may want to know more about what we do.</p>
 
                                   <div p-10>
-                                    <button class="btn btn-active btn-primary hover:text-red-500">button to do cool</button>
-                                    <button class="btn btn-active btn-secondary">website button</button>
+                                    {/* <button class="btn btn-active text-sky-100 bg-red-500 hover:bg-red-700">button to do cool</button> */}
                                 </div>
                               </div>
 
                           </div>
                       </div>
 
-                      <div className={"row-start-2"}>
+                      <div className={"row-start-2 py-4"}>
                           <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center flex-row-reverse justify-between">
                               <div className="avatar">
                                   <div className="w-72 rounded">
@@ -51,9 +48,11 @@ export default function Projects(){
                                     Rust Reticulum
                                   </h1>
 
-                                  <p class="text-2xl">
+                                  <p class="text-2xl py-8">
                                     Reticulum is designed to give communities and users a way of setting up peer-to-peer messaging networks that are secure, private, and extremely resilient. It runs over any kind of hardware --packet radio, LAN, wired connections, or even at the applications layer of TCP/IP
                                   </p>
+
+                                 
 
                                   <div p-10>
                                     
