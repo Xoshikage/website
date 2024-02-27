62d0a51e8884e14953ec7e14e52a90d80d5909a9
4ubiks <jaharris20@liberty.edu>
2024-02-27 09:04:36 -0500

    Making the projects page more epic

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,14 +3,16 @@ import Hero from "@/components/hero";
 
 export default function Projects(){
     return(
-        <main>
+        <main className="flex min-h-screen w-full flex-col items-center overflow-clip">
+
             <Navbar></Navbar>
+            
             <div class="w-full">      
             <Hero className={"min-h-screen h-full"}></Hero>         
                 
-            <div className={"bg-black w-full flex justify-center py-32"}>
+            <div className={"w-full flex justify-center py-32"}>
 
-                <div className={"w-11/12 max-w-[1500px] grid grid-rows-3 gap-3.5"}>
+                <div className={"w-11/12 max-w-[1000px] grid grid-rows-3 gap-3.5"}>
                     <div className={"row-start-1"}>
                         <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center">
                             <div className="avatar">
@@ -21,12 +23,12 @@ export default function Projects(){
 
                             <div className={"ml-8 h-full py-8 prose lg:prose-md"}>
                                 <h1>
-                                    <code>Club Website</code>
+                                    Club Website
                                 </h1>
 
 
                                 <p>
-                                <code>the most vital element of an organization is recognizability. we are currently in development of a visually pleasing, informative website that caters to anyone who may want to know more about what we do.</code>
+                                the most vital element of an organization is recognizability. we are currently in development of a visually pleasing, informative website that caters to anyone who may want to know more about what we do.
                                 </p>
                                     {/*<a href={"/projects"}/>*/}
                             </div>
@@ -38,7 +40,7 @@ export default function Projects(){
                 </div>
 
 
-                <div className={"w-11/12 max-w-[1500px] grid grid-rows-3 gap-3.5"}>
+                <div className={"w-11/12 max-w-[1000px] grid grid-rows-3 gap-3.5"}>
                     <div className={"row-start-1"}>
                         <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center">
                             <div className="avatar">
@@ -49,14 +51,14 @@ export default function Projects(){
 
                             <div className={"ml-8 h-full py-8 prose lg:prose-md"}>
                                 <h1>
-                                    <code>Rust Reticulum</code>
+                                    Rust Reticulum
                                 </h1>
 
 
                                 <p>
-                                    <code>Reticulum is designed to give communities and users a way of setting up peer-to-peer messaging networks that are secure, private, and extremely resilient. It runs over any kind of hardware --packet radio, LAN, wired connections, or even at the applications layer of TCP/IP</code>
+                                    Reticulum is designed to give communities and users a way of setting up peer-to-peer messaging networks that are secure, private, and extremely resilient. It runs over any kind of hardware --packet radio, LAN, wired connections, or even at the applications layer of TCP/IP
                                 </p>
-                                    {/*<a href={"/projects"}/>*/}
+                                <button class="btn btn-active btn-primary">BOTTON</button>
                             </div>
 
                                 <a href={"/projects"}>link</a>
