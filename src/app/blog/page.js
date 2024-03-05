18e82b5326545db31c23d68da1f4826632c411da
Xoshikage <162215505+Xoshikage@users.noreply.github.com>
2024-03-05 17:55:04 -0500

    i made a blog page

---
+++ src/app/blog/page.js
@@ -0,0 +1,14 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+
+export default function Projects(){
+    return(
+        <main className="">
+            <Navbar />
+
+            <Footer />
+
+        </main>
+    )   
+}
